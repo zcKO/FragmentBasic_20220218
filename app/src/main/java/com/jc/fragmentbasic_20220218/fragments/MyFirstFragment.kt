04d68d31885bbf58f3a1b3074883d3bf3443003a
JaeCheon <studyJcHub@gmail.com>
2022-02-18 15:17:29 +0900

    Fragment() 상속 -> xml 가져와서 연동

--- app/src/main/java/com/jc/fragmentbasic_20220218/fragments/MyFirstFragment.kt
+++ app/src/main/java/com/jc/fragmentbasic_20220218/fragments/MyFirstFragment.kt
@@ -1,4 +1,24 @@
 package com.jc.fragmentbasic_20220218.fragments
 
-class MyFirstFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jc.fragmentbasic_20220218.R
+
+class MyFirstFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // 여기서는 inflate 에 부가 정보를 주어야 한다.
+        // attachToRoot 해당 뷰가 root 에도 붙는지
+        return inflater.inflate(R.layout.fragment_my_first, container, false)
+
+    }
+
 }
